5039929a053dbd24169006c3302c2196af818a17
Sourav Abhishek <sourav_abhishek_ampba2022s@isb.edu>
2024-06-13 16:49:21 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI interview question generator Project',
+    version='0.0.0',
+    author="Sourav Abhishek",
+    author_email="sourav.abhishek@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+
+)
